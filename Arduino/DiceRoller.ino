c7ef6a00414e35bf9034fa3aa74609f340a720c8
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2018-12-13 23:30:08 -0800

    Initial skeleton

---
+++ Arduino/DiceRoller.ino
@@ -0,0 +1,96 @@
+#include <Servo.h>
+
+//---------------------------------------------------------------------------------------------
+
+static const int k_commandNone      = 0;
+static const int k_commandUp        = 1;
+static const int k_commandDown      = 2;
+static const int k_commandLoad      = 3;
+static const int k_commandCycle     = 4;
+static const int k_commandCycleDone = 5;
+
+static const int k_upDegrees      = 0;
+static const int k_downDegrees    = 180;
+static const int k_loadDegrees    = 95;
+
+static const int k_cycleTimeMs    = 3000;
+
+Servo g_servo;
+
+static unsigned long g_previousTimeMs = 0;
+
+static bool g_cycleUpNext = false;
+static unsigned long g_timeSinceCycleStartMs = k_cycleTimeMs;
+
+
+//---------------------------------------------------------------------------------------------
+
+void setup()
+{
+    Serial.begin(9600);
+    
+    g_servo.attach(9, 535, 1415);    
+    g_servo.write(k_loadDegrees);
+
+    g_previousTimeMs = millis();
+}
+
+void loop()
+{
+    // Detect rollover and handle somewhat gracefully
+    unsigned long currentTimeMs = millis();
+    unsigned long elapsedTimeMs = (currentTimeMs >= g_previousTimeMs) ? (currentTimeMs - g_previousTimeMs) : 0;
+    g_previousTimeMs = currentTimeMs;
+
+    // Cycle logic
+    if (g_timeSinceCycleStartMs < k_cycleTimeMs)
+    {
+        g_timeSinceCycleStartMs += elapsedTimeMs;
+        if (g_timeSinceCycleStartMs >= k_cycleTimeMs)
+            Serial.write(k_commandCycleDone);
+    }
+    
+    // Handle any serial input
+    while (Serial.available() > 0)
+    {
+        // One byte = one command for simplicity for now        
+        int command = Serial.read();
+        if (command >= 0)
+        {
+            if (command == k_commandUp)
+            {
+                g_timeSinceCycleStartMs = k_cycleTimeMs;
+                g_servo.write(k_upDegrees);
+            }
+            else if (command == k_commandDown)
+            {
+                g_timeSinceCycleStartMs = k_cycleTimeMs;
+                g_servo.write(k_downDegrees);
+            }
+            else if (command == k_commandLoad)
+            {
+                g_timeSinceCycleStartMs = k_cycleTimeMs;
+                g_servo.write(k_loadDegrees);
+            }
+            else if (command == k_commandCycle)
+            {
+                // Ignore if cycle is currently still happening
+                if (g_timeSinceCycleStartMs >= k_cycleTimeMs)
+                {                    
+                    if (g_cycleUpNext)
+                        g_servo.write(k_upDegrees);
+                    else
+                        g_servo.write(k_downDegrees);
+                    
+                    g_timeSinceCycleStartMs = 0;
+                    g_cycleUpNext = !g_cycleUpNext;
+                }
+            }
+        }
+    }
+
+    
+
+    // Don't really need to be hammering the CPU/latency for this use...
+    delay(10);
+}
---
+++ capture.py
@@ -0,0 +1,119 @@
+# Settings
+CAPTURE_DIR = 'data/2018-12-13/'
+CAPTURE_EXT = '.jpg'
+
+CAPTURE_BOX_START = (90, 90)
+CAPTURE_BOX_END = (840, 600)
+
+
+
+###################################################################################################
+import numpy as np
+import cv2
+import serial
+import os
+
+COMMAND_NONE       = 0
+COMMAND_UP         = 1
+COMMAND_DOWN       = 2
+COMMAND_LOAD       = 3
+COMMAND_CYCLE      = 4
+COMMAND_CYCLE_DONE = 5
+
+KEY_UP    = 2490368
+KEY_DOWN  = 2621440
+KEY_RIGHT = 2555904
+KEY_LEFT  = 2424832
+
+###################################################################################################
+
+class ArduinoSerial:
+	def __init__(self):
+		# NOTE: python -m serial.tools.list_ports
+		# Non-blocking mode (read can return 0)
+		self.m_serial = serial.Serial('COM7', timeout = 1)
+		#self.m_serial.write(b'180a')
+	
+	def __del__(self):
+		self.m_serial.close();
+
+	def write(self, command):
+		self.m_serial.write(command.to_bytes(1, byteorder='little'))
+
+	def readAvailable(self):
+		return self.m_serial.in_waiting > 0
+		
+	def read(self):
+		return int.from_bytes(self.m_serial.read(1), byteorder='little')
+
+
+
+###################################################################################################
+
+arduinoSerial = ArduinoSerial();
+
+cap = cv2.VideoCapture(cv2.CAP_DSHOW + 0)
+#cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+cap.set(cv2.CAP_PROP_EXPOSURE, -5.0)
+cap.set(cv2.CAP_PROP_FOCUS, 45)
+
+cv2.namedWindow('main1', cv2.WINDOW_AUTOSIZE)
+
+captureIndex = 0
+
+while (cv2.getWindowProperty('main1', 0) >= 0):
+	ret, frame = cap.read()
+	
+	display_image = frame.copy()
+	cv2.rectangle(display_image, CAPTURE_BOX_START, CAPTURE_BOX_END, (0,255,0), 1, 8, 0)
+	
+	cv2.imshow('main1', display_image)
+	key = cv2.waitKeyEx(1)
+	if (key >= 0):
+		#print(key)
+	
+		if key == ord('s'):
+			cv2.imwrite("test.png", frame)
+		elif key == KEY_UP:
+			arduinoSerial.write(COMMAND_UP)
+		elif key == KEY_DOWN:
+			arduinoSerial.write(COMMAND_DOWN)
+		elif key == KEY_RIGHT or key == KEY_LEFT:
+			arduinoSerial.write(COMMAND_LOAD)
+		elif key == ord(' '):
+			arduinoSerial.write(COMMAND_CYCLE)
+			
+	# Handle incoming commands
+	if (arduinoSerial.readAvailable()):
+		command = arduinoSerial.read();
+		#print(command)
+		if (command == COMMAND_CYCLE_DONE):
+			# TODO: Create unique path with date/time in it to avoid ever stomping anything
+			if not os.path.exists(CAPTURE_DIR):
+				os.makedirs(CAPTURE_DIR)
+				
+			fileName = os.path.join(CAPTURE_DIR, "{:06d}{}".format(captureIndex, CAPTURE_EXT))
+			print(fileName)
+	
+			cropped_frame = frame[CAPTURE_BOX_START[1]:CAPTURE_BOX_END[1], CAPTURE_BOX_START[0]:CAPTURE_BOX_END[0]]
+			
+			cv2.imwrite(fileName, cropped_frame)
+			
+			captureIndex += 1
+			
+			# Auto-continue with next cycle
+			arduinoSerial.write(COMMAND_CYCLE)
+	
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+###################################################################################################
+
+
+
