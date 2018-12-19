e22e1e11a69e08ea3942f959ae654417a46d38f9
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2018-12-18 21:25:10 -0800

    Reasonably good network results as a baseline

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 # TODO: Play with this - probably want to resize before dumping into the NN
 IMAGE_DIMENSIONS = 84
-
-showErrorImages = False
+BATCH_SIZE = 16
 
 import torch
 import torch.nn as nn
@@ -11,14 +10,14 @@ import torchvision.transforms
 import torch.utils.data
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL
+from imgaug import augmenters as iaa
+import imgaug
 
 def imshow(img):
 	img = img / 2 + 0.5     # unnormalize
 	npimg = img.numpy()
 	plt.imshow(np.transpose(npimg, (1, 2, 0)))
 	plt.show()
-
 
 class Unit(nn.Module):
 	def __init__(self, in_channels, out_channels):
@@ -37,16 +36,19 @@ class SimpleNet(nn.Module):
 	def __init__(self, num_classes):
 		super(SimpleNet,self).__init__()
 
-		self.unit1 = Unit(in_channels= 3, out_channels=32)
-		self.unit2 = Unit(in_channels=32, out_channels=32)
-		self.unit3 = Unit(in_channels=32, out_channels=32)
+		# NOTE: Probably a massively overkill network for our problem but it's fast and it works,
+		# so not much motivation to optimize it down at the moment.
+		
+		self.unit1 = Unit(in_channels=3, out_channels=4)
+		self.unit2 = Unit(in_channels=4, out_channels=4)
+		self.unit3 = Unit(in_channels=4, out_channels=4)
 
 		self.pool1 = nn.MaxPool2d(kernel_size = 2)
 
-		self.unit4 = Unit(in_channels=32, out_channels=64)
-		self.unit5 = Unit(in_channels=64, out_channels=64)
-		self.unit6 = Unit(in_channels=64, out_channels=64)
-		self.unit7 = Unit(in_channels=64, out_channels=64)
+		self.unit4 = Unit(in_channels=4, out_channels=8)
+		self.unit5 = Unit(in_channels=8, out_channels=8)
+		self.unit6 = Unit(in_channels=8, out_channels=8)
+		self.unit7 = Unit(in_channels=8, out_channels=8)
 
 		self.pool2 = nn.MaxPool2d(kernel_size = 2)
 		
@@ -56,11 +58,14 @@ class SimpleNet(nn.Module):
 		
 		#self.net = nn.Sequential(self.unit1, self.unit2, self.unit3, self.pool1)
 
-		self.fc = nn.Linear(in_features = int(16/4) * IMAGE_DIMENSIONS * IMAGE_DIMENSIONS, out_features = num_classes)
+		dimAfterPooling = int(IMAGE_DIMENSIONS / 4)		# Two 1/2 size pooling steps
+		self.fcSize = 4 * dimAfterPooling * dimAfterPooling
+		
+		self.fc = nn.Linear(in_features = self.fcSize, out_features = num_classes)
 
 	def forward(self, input):
 		output = self.net(input)
-		output = output.view(-1, int(16/4) * IMAGE_DIMENSIONS * IMAGE_DIMENSIONS)
+		output = output.view(-1, self.fcSize)
 		output = self.fc(output)
 		return output
 
@@ -95,9 +100,12 @@ def test(showErrorImages = False):
 def train(num_epochs):
 	best_acc = 0
 
-	for epoch in range(num_epochs):			
+	for epoch in range(num_epochs):
+		# Update epoch-based optimizer learning rate
 		scheduler.step()
+		
 		model.train()
+		
 		total = 0
 		train_acc = 0
 		train_loss = 0
@@ -105,16 +113,11 @@ def train(num_epochs):
 			# Move images and labels to gpu if available
 			images, labels = images.to(device), labels.to(device)
 
-			# Clear all accumulated gradients
 			optimizer.zero_grad()
-			# Predict classes using images from the test set
 			outputs = model(images)
-			# Compute the loss based on the predictions and actual labels
 			loss = loss_fn(outputs,labels)
-			# Backpropagate the loss
 			loss.backward()
 
-			# Adjust parameters according to the computed gradients
 			optimizer.step()
 
 			train_loss += loss.item() * images.size(0)
@@ -123,36 +126,52 @@ def train(num_epochs):
 			_, predicted = torch.max(outputs.data, 1)
 			train_acc += (predicted == labels).sum().item()
 
-		# Compute the average acc and loss over all 50000 training images
+		# Compute the accuracy and loss
 		train_acc = train_acc / total
 		train_loss = train_loss / total
 
-		test_acc = test(showErrorImages)
+		test_acc = test()
 
 		# Save the model if the test acc is greater than our current best
 		if test_acc > best_acc:
 			save_models(epoch)
 			best_acc = test_acc
 
-		# Print the metrics
 		print("Epoch {}, Train Accuracy: {} , TrainLoss: {} , Test Accuracy: {:.5f}".format(epoch, train_acc, train_loss, test_acc))
 
+class ImgAugTrainTransform:
+  def __init__(self):
+    self.aug = iaa.Sequential([
+        iaa.Affine(
+            scale = {"x": (0.9, 1.1), "y": (0.9, 1.1)},
+            translate_percent = {"x": (-0.05, 0.05), "y": (-0.05, 0.05)},
+            rotate = (0, 360),
+            order = 1,
+            cval = (0, 255),
+        ),
+		iaa.Sometimes(0.25, iaa.GaussianBlur(sigma = [1.0, 2.0])),
+		iaa.AddToHueAndSaturation((-10, 10)),
+		#iaa.AdditiveGaussianNoise(loc = 0, scale = (0.0, 0.05*255), per_channel = 0.5)
+    ])
+      
+  def __call__(self, img):
+    img = np.array(img)
+    return self.aug.augment_image(img)
+		
 if __name__ == "__main__":
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	print(device)
-
-	batch_size = 16
 	
 	# Training data
 	trainTransform = torchvision.transforms.Compose([
-		torchvision.transforms.ColorJitter(0.2, 0.2, 0.05, 0.05),
-		torchvision.transforms.RandomAffine(180, scale = [0.9, 1.1], resample = PIL.Image.BILINEAR),
+		ImgAugTrainTransform(),
 		torchvision.transforms.ToTensor(),
 		torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 	])
+	
 	trainSet = torchvision.datasets.ImageFolder(root='training_data/xwing_red', transform = trainTransform)	
 	print("Raw training set size: {}".format(len(trainSet)))
-	trainLoader = torch.utils.data.DataLoader(trainSet, batch_size = batch_size, shuffle = True, num_workers = 4)
+	trainLoader = torch.utils.data.DataLoader(trainSet, batch_size = BATCH_SIZE, shuffle = True, num_workers = 4)
 	
 	classLabelStrings = trainSet.classes
 	print("Classes: {}".format(classLabelStrings))
@@ -164,22 +183,21 @@ if __name__ == "__main__":
 	])
 	testSet = torchvision.datasets.ImageFolder(root='test_data/xwing_red', transform = testTransform)	
 	print("Test set size: {}".format(len(testSet)))
-	testLoader  = torch.utils.data.DataLoader(testSet,  batch_size = batch_size, shuffle = False, num_workers = 4)
+	testLoader  = torch.utils.data.DataLoader(testSet,  batch_size = BATCH_SIZE, shuffle = False, num_workers = 4)
 
 
 	#trainCount = int(0.8 * datasetCount)
 	#trainSet, testSet = torch.utils.data.random_split(dataset, [trainCount, datasetCount - trainCount])
 	
 	# DEBUG: Show some images
-	#images, labels = iter(trainLoader).next()
-	#print(' '.join('%5s' % trainSet.classes[labels[j]] for j in range(4)))
-	#imshow(torchvision.utils.make_grid(images[0:16], nrow = 4))
+	images, labels = iter(trainLoader).next()
+	print(' '.join('%5s' % trainSet.classes[labels[j]] for j in range(4)))
+	imshow(torchvision.utils.make_grid(images[0:16], nrow = 4))
 		
 	# Create model, optimizer and loss function
 	model = SimpleNet(num_classes = len(trainSet.classes))
 	model.to(device)
 
-	# 0.0001 default learning rate
 	optimizer = torch.optim.SGD(model.parameters(), lr = 0.01, momentum = 0.9)
 	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 30,  gamma = 0.1)
 	loss_fn = nn.CrossEntropyLoss()
