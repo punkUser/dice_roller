32b72576ca1333d9f2ec27ecda6ca47ac4a4e36d
Andrew Lauritzen <andrew.lauritzen@gmail.com>
2018-12-19 12:41:19 -0800

    Fix some training data and train an initial model

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 # Settings
 TRAIN_DATA = 'training_data/xwing_red'
 TEST_DATA  = 'test_data/xwing_red'
+OUTPUT_MODEL_FILE = 'output/xwing_red.tar'
 
 # TODO: Play with this - probably want to resize before dumping into the NN
 IMAGE_DIMENSIONS = 84
@@ -21,7 +22,7 @@ from imgaug import augmenters as iaa
 import imgaug
 
 def imshow(img):
-	img = img / 2 + 0.5     # unnormalize
+	img = img / 2 + 0.5		# unnormalize
 	npimg = img.numpy()
 	plt.imshow(np.transpose(npimg, (1, 2, 0)))
 	plt.show()
@@ -39,9 +40,9 @@ class ConvUnit(nn.Module):
 		output = self.bn(output)
 		return output
 
-class SimpleNet(nn.Module):
+class Net(nn.Module):
 	def __init__(self, num_classes):
-		super(SimpleNet,self).__init__()
+		super(Net, self).__init__()
 
 		# NOTE: Probably a overkill network for our problem but it's fast and it works,
 		# so not much motivation to optimize it down at the moment.
@@ -83,112 +84,136 @@ class SimpleNet(nn.Module):
 		output = self.fc(output)
 		return output
 
-def save_models(epoch):
-	#torch.save(model.state_dict(), "cifar10model_{}.model".format(epoch))
-	#print("Checkpoint saved")
-	pass
+class Model:
+	def __init__(self, classLabelStrings):
+		self.classLabelStrings = classLabelStrings
+	
+		self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		print("Device: {}".format(self.device))
+					
+		# Create model, optimizer and loss function
+		self.model = Net(num_classes = len(self.classLabelStrings))
+		self.model.to(self.device)
 
-def test(showErrorImages = False):
-	model.eval()
-	correct = 0
-	total = 0
-	for i, (images, labels) in enumerate(testLoader):	
-		images, labels = images.to(device), labels.to(device)
-
-		# Predict classes using images from the test set
-		outputs = model(images)
-		_,predicted = torch.max(outputs.data, 1)
-		total += labels.size(0)
-		correct += (predicted == labels).sum().item()
-		
-		# Debug which ones are failing
-		if showErrorImages:
-			for i in range(len(predicted)):
-				if predicted[i] != labels[i]:
-					print("Predicted {}, expected {}. Weights {}".format(classLabelStrings[predicted[i]], classLabelStrings[labels[i]], outputs.data[i]))
-					imshow(images[i].cpu())
-
-	test_acc = correct / total
-	return test_acc
-
-def train(num_epochs):
-	best_acc = 0
-
-	for epoch in range(num_epochs):
-		# Update epoch-based optimizer learning rate
-		scheduler.step()
-		
-		model.train()
-		
-		total = 0
-		train_acc = 0
-		train_loss = 0
-		for i, (images, labels) in enumerate(trainLoader):
-			# Move images and labels to gpu if available
-			images, labels = images.to(device), labels.to(device)
-
-			optimizer.zero_grad()
-			outputs = model(images)
-			loss = loss_fn(outputs,labels)
-			loss.backward()
-
-			optimizer.step()
-
-			train_loss += loss.item() * images.size(0)
+		self.optimizer    = torch.optim.SGD(self.model.parameters(), lr = 0.01, momentum = 0.9)
+		self.scheduler    = torch.optim.lr_scheduler.StepLR(self.optimizer, 20,	gamma = 0.1)
+		self.lossFunction = nn.CrossEntropyLoss()
 			
+	def save(self, fileName = OUTPUT_MODEL_FILE):
+		torch.save({
+			'epoch': self.epoch,
+			'model_state_dict': self.model.state_dict(),
+			'optimizer_state_dict': self.optimizer.state_dict(),
+			'scheduler_state_dict': self.scheduler.state_dict(),
+			'loss': self.lossFunction,
+			}, fileName)
+		#print('Model saved to {}'.format(OUTPUT_MODEL_FILE))
+		
+	def load(self, fileName = OUTPUT_MODEL_FILE):
+		checkpoint = torch.load(OUTPUT_MODEL_FILE)
+		
+		self.epoch = checkpoint['epoch']
+		self.model.load_state_dict(checkpoint['model_state_dict'])
+		self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+		self.scheduler.load_state_dict(checkpoint['scheduler_state_dict'])		
+		self.lossFunction = checkpoint['loss']
+
+	def test(self, loader, showErrorImages = False):
+		self.model.eval()
+		correct = 0
+		total = 0
+		for i, (images, labels) in enumerate(loader):
+			images, labels = images.to(self.device), labels.to(self.device)
+
+			# Predict classes using images from the test set
+			outputs = self.model(images)
+			_,predicted = torch.max(outputs.data, 1)
 			total += labels.size(0)
-			_, predicted = torch.max(outputs.data, 1)
-			train_acc += (predicted == labels).sum().item()
+			correct += (predicted == labels).sum().item()
+			
+			# Debug which ones are failing
+			if showErrorImages:
+				# DEBUG
+				#for i in range(len(predicted)):
+				#	maxRow = torch.max(outputs.data[i])
+				#	row = outputs.data[i] / maxRow
+				#	print("{}, max {}".format(row, maxRow))
+					
+				for i in range(len(predicted)):
+					if predicted[i] != labels[i]:
+						print("Index ~{} Predicted {}, expected {}. Weights {}".format(total, classLabelStrings[predicted[i]], classLabelStrings[labels[i]], outputs.data[i]))
+						imshow(images[i].cpu())
 
-		# Compute the accuracy and loss
-		train_acc = train_acc / total
-		train_loss = train_loss / total
+		test_acc = correct / total
+		return test_acc
+		
+	def train(self, numEpochs, trainLoader, testLoader):		
+		for epoch in range(numEpochs):
+			self.epoch = epoch
+			
+			# Update epoch-based optimizer learning rate
+			self.scheduler.step()
+			self.model.train()
+			
+			total = 0
+			trainAcc = 0
+			trainLoss = 0
+			for i, (images, labels) in enumerate(trainLoader):
+				# Move images and labels to gpu if available
+				images, labels = images.to(self.device), labels.to(self.device)
 
-		test_acc = test()
+				self.optimizer.zero_grad()
+				outputs = self.model(images)
+				loss = self.lossFunction(outputs,labels)
+				loss.backward()
 
-		# Save the model if the test acc is greater than our current best
-		if test_acc > best_acc:
-			save_models(epoch)
-			best_acc = test_acc
+				self.optimizer.step()
 
-		print("Epoch {}, Train Accuracy: {} , TrainLoss: {} , Test Accuracy: {:.5f}".format(epoch, train_acc, train_loss, test_acc))
+				trainLoss += loss.item() * images.size(0)
+				
+				total += labels.size(0)
+				_, predicted = torch.max(outputs.data, 1)
+				trainAcc += (predicted == labels).sum().item()
+
+			# Compute the accuracy and loss
+			trainAcc = trainAcc / total
+			trainLoss = trainLoss / total
+
+			testAcc = self.test(testLoader)
+			print("Epoch {}, Train Accuracy: {:.5f} , TrainLoss: {} , Test Accuracy: {:.5f}".format(epoch, trainAcc, trainLoss, testAcc))
+			
+			# Save checkpoint
+			self.save()
 
 class ImgAugTrainTransform:
-  def __init__(self):
-    self.aug = iaa.Sequential([
-        iaa.Affine(
-            scale = {"x": (0.9, 1.1), "y": (0.9, 1.1)},
-            translate_percent = {"x": (-0.05, 0.05), "y": (-0.05, 0.05)},
-            rotate = (0, 360),
-            order = 1,
-            cval = (0, 255),
-        ),
-		iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=[1.0, 1.8])),
-		iaa.AddToHueAndSaturation((-10, 10)),
-		#iaa.AdditiveGaussianNoise(loc = 0, scale = (0.0, 0.05*255), per_channel = 0.5)
-    ])
-      
-  def __call__(self, img):
-    img = np.array(img)
-    return self.aug.augment_image(img)
-		
-if __name__ == "__main__":
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	print(device)
-	
+	def __init__(self):
+		self.aug = iaa.Sequential([
+			iaa.Affine(
+				scale = {"x": (0.9, 1.1), "y": (0.9, 1.1)},
+				translate_percent = {"x": (-0.05, 0.05), "y": (-0.05, 0.05)},
+				rotate = (0, 360),
+				order = 1,
+				cval = (0, 255),
+			),
+			iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=[1.0, 1.8])),
+			iaa.AddToHueAndSaturation((-10, 10)),
+			#iaa.AdditiveGaussianNoise(loc = 0, scale = (0.0, 0.05*255), per_channel = 0.5)
+		])
+
+	def __call__(self, img):
+		img = np.array(img)
+		return self.aug.augment_image(img)
+
+
+if __name__ == "__main__":	
 	# Training data
 	trainTransform = torchvision.transforms.Compose([
 		ImgAugTrainTransform(),
 		torchvision.transforms.ToTensor(),
 		torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-	])
-	
-	trainSet = torchvision.datasets.ImageFolder(root=TRAIN_DATA, transform=trainTransform)	
-	print("Raw training set size: {}".format(len(trainSet)))
+	])	
+	trainSet = torchvision.datasets.ImageFolder(root=TRAIN_DATA, transform=trainTransform)
 	trainLoader = torch.utils.data.DataLoader(trainSet, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-	
-	classLabelStrings = trainSet.classes
-	print("Classes: {}".format(classLabelStrings))
 	
 	# Test data
 	testTransform = torchvision.transforms.Compose([
@@ -196,27 +221,29 @@ if __name__ == "__main__":
 		torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 	])
 	testSet = torchvision.datasets.ImageFolder(root=TEST_DATA, transform=testTransform)	
+	testLoader	= torch.utils.data.DataLoader(testSet,	batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
+
+	classLabelStrings = trainSet.classes
+	print("Classes: {}".format(classLabelStrings))
+	print("Raw training set size: {}".format(len(trainSet)))
 	print("Test set size: {}".format(len(testSet)))
-	testLoader  = torch.utils.data.DataLoader(testSet,  batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
-
-
-	#trainCount = int(0.8 * datasetCount)
-	#trainSet, testSet = torch.utils.data.random_split(dataset, [trainCount, datasetCount - trainCount])
 	
 	# DEBUG: Show some images
 	images, labels = iter(trainLoader).next()
-	print(' '.join('%5s' % trainSet.classes[labels[j]] for j in range(4)))
+	print(' '.join('%5s' % classLabelStrings[labels[j]] for j in range(4)))
 	imshow(torchvision.utils.make_grid(images[0:16], nrow = 4))
 		
-	# Create model, optimizer and loss function
-	model = SimpleNet(num_classes = len(trainSet.classes))
-	model.to(device)
-
-	optimizer = torch.optim.SGD(model.parameters(), lr = 0.01, momentum = 0.9)
-	scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 20,  gamma = 0.1)
-	loss_fn = nn.CrossEntropyLoss()
-
-	train(20)
+	diceModel = Model(classLabelStrings)
+	diceModel.load() # Continue onwards!
+	#diceModel.train(30, trainLoader, testLoader)
 	
 	# Final test and display of mispredicted ones
-	test_acc = test(True)
+	testAcc = diceModel.test(testLoader, True)
+	print("Test Set Accuracy: {}".format(testAcc))
+	
+	# Now test all the training ones too, but without the augmentation
+	rawTrainSet = torchvision.datasets.ImageFolder(root=TRAIN_DATA, transform=testTransform)
+	rawTrainLoader = torch.utils.data.DataLoader(rawTrainSet, batch_size=BATCH_SIZE, shuffle=False, num_workers=4)
+	rawTrainAcc = diceModel.test(rawTrainLoader, True)
+	print("Raw trainT Set Accuracy: {}".format(rawTrainAcc))
+	
